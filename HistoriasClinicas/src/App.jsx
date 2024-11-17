6008691af5d1d9be142dc4396bda2981a9de6aa1
Cristian Lucero/ <143764439+Cris7ian-tech@users.noreply.github.com>
2024-11-17 04:53:00 -0300

    Add files via upload

--- HistoriasClinicas/src/App.jsx
+++ HistoriasClinicas/src/App.jsx
@@ -1,52 +1,30 @@
-import './App.css'
-import { useState, useEffect } from 'react'
-import PacientesForm from './components/PacientesForm'
-import PacientesLista from './components/PacientesLista'
+import PatientsProvider from "./components/Context/patientsProvider";
+import PacientesForm from "./components/PacientesForm";
+import PacientesLista from "./components/PacientesLista";
+import SidebarMenu from "./components/Sidebar/SidebarMenu";
+import TablaPacientes from "./components/TablaPacientes";
+import Navbar from "./components/Navbar/Navbar";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+
 function App() {
-  const [pacientes, setPacientes] = useState([])
-  const [editandoIndex, setEditandoIndex] = useState(null)
-
-  //intento de implementacion LocalStorage
-  useEffect(() => {
-    const pacientesAlmacenados = localStorage.getItem('pacientes')
-    if (pacientesAlmacenados) {
-      setPacientes(JSON.parse(pacientesAlmacenados))
-    }
-  }, []);
-  //a ver si guarda...
-  // useEffect(() => {
-  //   localStorage.setItem('pacientes', JSON.stringify(pacientes))  
-  // }, [pacientes]);
-
-  const handleAgregarPaciente = (nuevoPaciente) => {
-    if (editandoIndex !== null) {
-      const subirPacientes = pacientes.map((paciente, index) => 
-        index === editandoIndex ? nuevoPaciente : paciente
-      )
-      setPacientes(subirPacientes)
-      setEditandoIndex(null)
-      } else {
-        setPacientes([...pacientes, nuevoPaciente])
-      }
-    }
   
-  const handleEditarPaciente = (index) => {
-    setEditandoIndex(index);
-  }
-  
+
   return (
     <>
-      <div className="container">
-        <h1>Demo Manejo de Pacientes</h1>
-        <PacientesForm 
-        agregarPaciente={handleAgregarPaciente}
-        editandoPaciente={editandoIndex !== null ? pacientes[editandoIndex] : null}
-        />
-        <PacientesLista pacientes={pacientes} editarPaciente={handleEditarPaciente}
-        />
-      </div>
+    <PatientsProvider>
+      <Navbar />
+      <h2>Arranco!!</h2>
+      <h1>Demo Manejo de Pacientes</h1>
+      <PacientesForm />
+      <PacientesLista />
+      <SidebarMenu />
+    </PatientsProvider>
+    <TablaPacientes />  
     </>
-  )
+  );
 }
 
-export default App
+export default App;
