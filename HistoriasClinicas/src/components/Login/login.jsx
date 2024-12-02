e132f57ca1852c1e0b6917a6094f190ef737b589
Cristian Lucero/ <cristian_ala2@hotmail.com>
2024-12-02 03:26:10 -0300

    loguin con estilos de landing-page, falta efecto el los input

--- HistoriasClinicas/src/components/Login/login.jsx
+++ HistoriasClinicas/src/components/Login/login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom"; // Importa useNavigate
 import "./login.css";
 
@@ -30,48 +30,61 @@ const Login = ({ onLoginExitoso }) => {
   };
 
   return (
-    <div className="form">
-      <h3 className="inicios">Iniciar Sesión</h3>
-      <form onSubmit={handleLogin} className="formulario">
-        <div className="input-group">
-          <label htmlFor="email" className="form-label">
+    <>
+    <div className="form-loguin-contenedor">
+
+      <form onSubmit={handleLogin} className="formulario-loguin">
+      <h3 className="form-loguin-title">Iniciar Sesión</h3>
+
+        <div className="form-loguin-input-group">
+          <label htmlFor="email" className="form-loguin-label">
             Email:
           </label>
           <input
             type="email"
             id="email"
-            className="form-control"
-            placeholder="Ingrese su email"
+            className="form-control-loguin"
+            placeholder=""
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
-        <div className="input-group">
-          <label htmlFor="password" className="form-label">
+        <div className="form-loguin-input-group">
+          <label htmlFor="password" className="form-loguin-label">
             Contraseña:
           </label>
           <input
             type="password"
             id="password"
-            className="form-control"
-            placeholder="Ingrese su contraseña"
+            className="form-control-loguin"
+            placeholder=""
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
 
-        <button type="submit" className="btn btn-success mt-3">
+        <button type="submit" className="buton-loguin-iniciar-sesion">
           Iniciar Sesión
         </button>
       </form>
-      <div className="registrarse mt-4">
-        <p>¿No tienes una cuenta?</p>
-        <button onClick={handleNavigateToInfo} className="btn btn-primary">
+      <div className="registro-loguin">
+        <p className="registro-loguin-text">¿No tienes una cuenta?</p>
+        <button onClick={handleNavigateToInfo} className="button-registo-loguin">
           Registrarse
         </button>
       </div>
     </div>
+
+    <div className="wave-loguin">
+    <svg className="wave-loguin-end" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 260"><path fill="#fff" d="M0,32L120,74.7C240,117,480,203,720,234.7C960,267,1200,245,1320,234.7L1440,224L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path></svg>
+    </div>
+
+    
+    </>
+
+    
+    
   );
 };
 
--- HistoriasClinicas/src/components/Registrarse/Registrarse.jsx
+++ HistoriasClinicas/src/components/Registrarse/Registrarse.jsx
@@ -20,7 +20,7 @@ const Registrarse = ({ onCambiarVista }) => {
 
   return (
     <>
-    <section className="section-formulario-registro">
+    <div className="section-formulario-registro">
 
       <form onSubmit={handleRegister} className="formulario-registro">
       <h3 className="formulario-registrarse-title">Registrarse</h3>
@@ -40,7 +40,7 @@ const Registrarse = ({ onCambiarVista }) => {
           />
         </div>
 
-        <div className="input-group">
+        <div className="form-input-group">
           <label htmlFor="email" className="form-label">
             Email:
           </label>
@@ -54,7 +54,7 @@ const Registrarse = ({ onCambiarVista }) => {
           />
         </div>
 
-        <div className="input-group">
+        <div className="form-input-group">
           <label htmlFor="password" className="form-label">
             Contraseña:
           </label>
@@ -66,18 +66,16 @@ const Registrarse = ({ onCambiarVista }) => {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-        </div>
-
-        <button type="submit" className="button-registrarse">
+        <button type="submit" className="boton-form-registrarse">
           Registrarse
         </button>
         </div>
+        </div>
       </form>
+    </div>
 
-
-    </section>
       <div className="wave-form">
-      <svg className="wave-form-end" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" d="M0,288L48,272C96,256,192,224,288,197.3C384,171,480,149,576,165.3C672,181,768,235,864,250.7C960,267,1056,245,1152,250.7C1248,256,1344,288,1392,304L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+      <svg className="wave-form-end" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 320"><path fill="#fff" d="M0,288L48,272C96,256,192,224,288,197.3C384,171,480,149,576,165.3C672,181,768,235,864,250.7C960,267,1056,245,1152,250.7C1248,256,1344,288,1392,304L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
       </div>
     </>
   );
