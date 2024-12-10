f43175e2a6f5f4e75fca69c5a1d10c416e01b7cb
santino <santinofernandez67@gmail.com>
2024-12-10 10:15:16 -0300

    cambios

--- client/src/ProtectedRoute.jsx
+++ client/src/ProtectedRoute.jsx
@@ -1,8 +1,9 @@
 import {Navigate, Outlet} from 'react-router-dom'
 import {useAuth} from "./components/Context/AuthContext"
 function ProtectedRoute(){
-    const {user,isAuthenticated}=useAuth()
-    if(!isAuthenticated) return <Navigate to='/login' replace/>
+    const {loading,isAuthenticated}=useAuth()
+    if(loading) return <h1>Loading...</h1>
+    if(!loading && !isAuthenticated) return <Navigate to='/login' replace/>
     return(
         <Outlet/>
     )
