f9fd314311f38e293a2f39a1dc52005c1e320028
santinofernandez14 <141685479+santinofernandez14@users.noreply.github.com>
2024-12-30 17:00:41 -0300

    cambios

--- client/src/pages/errorpage/ErrorPage.jsx
+++ client/src/pages/errorpage/ErrorPage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./errorpage.css";
-import errorImage from "../../uttils/imagesErrors/img1.png";
 import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
@@ -8,7 +7,6 @@ const ErrorPage = () => {
     <section className="error-page">
       <div className="error-content">
         <h3 className="formulario-registrarse-title">Oops... Error 404</h3>
-        <img src={errorImage} alt="Error" className="error-image" />
         <p>La página que buscas no existe o hubo un error inesperado.</p>
         <Link to="/" className="btn btn-primary">
           Volver al inicio
