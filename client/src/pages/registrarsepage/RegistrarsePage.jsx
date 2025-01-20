78963a7b69b5cdf01921ccdb0ff968a48de76660
santinofernandez14 <141685479+santinofernandez14@users.noreply.github.com>
2025-01-20 16:35:09 -0300

    cambios

--- client/src/pages/registrarsepage/RegistrarsePage.jsx
+++ client/src/pages/registrarsepage/RegistrarsePage.jsx
@@ -23,63 +23,77 @@ const RegistrarsePage = () => {
 
   return (
     <>
-    <section className="section-formulario-registro">
-      <form className="formulario-registro" onSubmit={onSubmit}>
-        <h3 className="formulario-registrarse-title">Registrarse</h3>
+      <section className="section-formulario-registro">
+        <form className="formulario-registro" onSubmit={onSubmit}>
+          <h3 className="formulario-registrarse-title">Registrarse</h3>
 
-        {/* Mostrar errores del backend aquí */}
-        {registerErrors.length > 0 && (
-          <div className="errores-backend">
-            {registerErrors.map((error, i) => (
-              <p key={i} className="mensaje-error-backend">
-                {error}
-              </p>
-            ))}
+          {/* Mostrar errores del backend aquí */}
+          {registerErrors.length > 0 && (
+            <div className="errores-backend">
+              {registerErrors.map((error, i) => (
+                <p key={i} className="mensaje-error-backend">
+                  {error}
+                </p>
+              ))}
+            </div>
+          )}
+
+          <div className="formulario-contenedor">
+            <div
+              className={`form-input-group ${
+                errors.username ? "has-error" : ""
+              }`}
+            >
+              <input
+                type="text"
+                placeholder="Nombre"
+                {...register("username", { required: true })}
+                className="form-control"
+              />
+              {errors.username && (
+                <p className="mensaje-error">
+                  El nombre de usuario es requerido
+                </p>
+              )}
+            </div>
+
+            <div
+              className={`form-input-group ${errors.email ? "has-error" : ""}`}
+            >
+              <input
+                type="email"
+                placeholder="Email"
+                {...register("email", { required: true })}
+                className="form-control"
+              />
+              {errors.email && (
+                <p className="mensaje-error">El email es requerido</p>
+              )}
+            </div>
+
+            <div
+              className={`form-input-group ${
+                errors.password ? "has-error" : ""
+              }`}
+            >
+              <input
+                type="password"
+                placeholder="Contraseña"
+                {...register("password", { required: true })}
+                className="form-control"
+              />
+              {errors.password && (
+                <p className="mensaje-error">La contraseña es requerida</p>
+              )}
+            </div>
+
+            <button type="submit" className="button-registrarse">
+              Registrarse
+            </button>
           </div>
-        )}
-
-        <div className="formulario-contenedor">
-          <div className={`form-input-group ${errors.username ? "has-error" : ""}`}>
-            <input
-              type="text"
-              placeholder="Nombre"
-              {...register("username", { required: true })}
-              className="form-control"
-            />
-            {errors.username && (
-              <p className="mensaje-error">El nombre de usuario es requerido</p>
-            )}
-          </div>
-
-          <div className={`form-input-group ${errors.email ? "has-error" : ""}`}>
-            <input
-              type="email"
-              placeholder="Email"
-              {...register("email", { required: true })}
-              className="form-control"
-            />
-            {errors.email && <p className="mensaje-error">El email es requerido</p>}
-          </div>
-
-          <div className={`form-input-group ${errors.password ? "has-error" : ""}`}>
-            <input
-              type="password"
-              placeholder="Contraseña"
-              {...register("password", { required: true })}
-              className="form-control"
-            />
-            {errors.password && (
-              <p className="mensaje-error">La contraseña es requerida</p>
-            )}
-          </div>
-
-          <button type="submit" className="button-registrarse">
-            Registrarse
-          </button>
-        </div>
-      </form>
-    </section>
-  </>
+        </form>
+      </section>
+    </>
   );
 };
 
